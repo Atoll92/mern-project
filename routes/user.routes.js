30f4c8a177af7fe5e652a3771afdfea6a3aebc51
Arthur Boval <arthurboval@MacBook-Pro-de-Arthur-2.local>
2021-12-29 19:42:27 +0100

    Success!

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,9 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 
-router.post("/register", function(req,res)
-{ authController.signUP });
-// router.post("/register", authController.signUp);
+// router.post("/register", function(req,res)
+// { authController.signUp });
+router.post("/register", authController.signUp);
 // router.post('/login', authController.signIn);
 // router.get('/logout', authController.logout);
 // router.post("/register", authController.signUp);
